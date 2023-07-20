9fcea767311f3abd28c6a4dbc7cf11e996a5df6b
Cauê catone silva <cauecatonesilva@gmail.com>
2023-07-20 14:45:17 -0400

    add: adicionar projeto interdiciplinar Realizado para faculdade

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -90,38 +90,20 @@ function projetos(section) {
         <p>HTML - CSS - javascript</p>
       </div>
 
-        <div class="card">
-          <h3>Em breve...</h3>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path
-              d="M5 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm14 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-7 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
-              fill="rgba(255,255,255,1)"
-            />
-          </svg>
-          <p>
-            Aprendizado constante! <br />
-            Em breve muitos outros projetos.
-          </p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path fill="none" d="M0 0H24V24H0z" />
-            <path
-              d="M21 3v2c0 9.627-5.373 14-12 14H5.243C5.08 19.912 5 20.907 5 22H3c0-1.363.116-2.6.346-3.732C3.116 16.974 3 15.218 3 13 3 7.477 7.477 3 13 3c2 0 4 1 8 0zm-8 2c-4.418 0-8 3.582-8 8 0 .362.003.711.01 1.046 1.254-1.978 3.091-3.541 5.494-4.914l.992 1.736C8.641 12.5 6.747 14.354 5.776 17H9c6.015 0 9.871-3.973 9.997-11.612-1.372.133-2.647.048-4.22-.188C13.627 5.027 13.401 5 13 5z"
-              fill="rgba(255,255,255,1)"
-            />
-          </svg>
+      <div class="card">
+      <h3>projeto Fasiclin</h3>
+      <div class="img-wrapper">
+        <img
+          src="./assets/images/projects/projetoFasiclin.gif"
+        />
+        <div class="buttons">
         </div>
-        <div></div>
+      </div>
+      <p> 
+      layout desenvolvido para uso na faculdade como projeto interdiciplinar, Trazendo funções da area de vendas da clinica, captando agenda de cliente, realizando pagamento, trazendo recibo de venda, e gerando relatorio 
+      </p>
+      <p>HTML - CSS - ReactJs, Nodejs, mySQL</p>
+    </div>
       </div>
     </div>`;
 }
